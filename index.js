8f21895ec1d0f6cbe7b4ffa578e61a78f639897a
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-05-18 19:39:49 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const menu = document.getElementsByClassName("nav");
 const toggleButton = document.getElementById("Realtoggle");
-toggleButton.addEventListener('click',function(){
-    menu[0].classList.toggle("mobile-nav");
-    menu[0].classList.remove("nav");
+const CrossButton = document.getElementById("cross");
+const Main = document.getElementById("MenuMobile");
+
+
+toggleButton.addEventListener('click',function(){ 
+    Main.classList.add("mobile-nav");
+    Main.classList.remove("nav");
+    toggleButton.classList.add("nav");
+})
+
+
+CrossButton.addEventListener('click',function(){
+   Main.classList.remove("mobile-nav");
+   Main.classList.add("nav");
+    toggleButton.classList.remove("nav");
+
 })
